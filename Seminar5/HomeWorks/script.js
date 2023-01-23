dfc69f27ba851ef23794bd289924a4c661b576b8
vasiliy <vasilniy@rambler.ru>
2023-01-23 22:43:12 +0600

    ДЗ №5 к Семинару 5

--- Seminar5/HomeWorks/script.js
+++ Seminar5/HomeWorks/script.js
@@ -20,34 +20,35 @@ for (const key in numbers) {
 */
 
 /*
-2
-Необходимо из объекта, который лежит в константе post вывести значения, к
-которым приписан комментарий, в консоль.
+2. Необходимо из объекта, который лежит в константе post вывести значения, к
+которым приписан комментарий, в консоль. */
 
 const post = {
-author: "John", // вывести этот текст
-postId: 23,
-comments: [
-{
-userId: 10,
-userName: "Alex",
-text: "lorem ipsum",
-rating: {
-likes: 10,
-dislikes: 2, // вывести это число
-},
-},
-{
-userId: 5, // вывести это число
-userName: "Jane",
-text: "lorem ipsum 2", // вывести этот текст
-rating: {
-likes: 3,
-dislikes: 1,
-},
-},
-],
-};              */
+    author: "John", // вывести этот текст
+    postId: 23,
+    comments: [
+      {
+        userId: 10,
+        userName: "Alex",
+        text: "lorem ipsum",
+        rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+        },
+      },
+      {
+        userId: 5, // вывести это число
+        userName: "Jane",
+        text: "lorem ipsum 2", // вывести этот текст
+        rating: {
+        likes: 3,
+        dislikes: 1,
+        },
+      },
+    ],
+}             
+
+*/
 
 /*
 3 Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя
