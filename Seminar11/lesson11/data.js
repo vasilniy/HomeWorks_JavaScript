b2dab2ed17a38a65c89ae62df783d221e23db0db
vasiliy <vasilniy@rambler.ru>
2023-02-12 10:22:48 +0600

    ДЗ №10 к Семинару 10

---
+++ Seminar11/lesson11/data.js
@@ -0,0 +1,37 @@
+// Дан макет https://www.figma.com/file/mZwLT9fKktsWuVyfQRoST1/shop-(Copy)-(Copy)?node-id=73%3A140 в котором представлены товары на странице корзины
+// Необходимо создать файл data.js в котором создать переменную dataProducts в которую присвоить JSON данные по товарам.
+// Вам нужно самостоятельно создать JSON данные (не забыть про кавычки для ключей и значений
+// // Добавить все данные из макета, чтобы в дальнейшим по ним мы смогли создать вёрстку
+
+const dataProductsJson = `[
+  {
+    "name": "MANGO  PEOPLE  T-SHIRT",
+    "price": "300",
+    "color": "Red",
+    "size": "XL",
+    "quantity": "2",
+    "img": "img/img_1.jpg"
+  },  {
+    "name": "MANGO  PEOPLE  T-SHIRT",
+    "price": "300",
+    "color": "Red",
+    "size": "XL",
+    "quantity": "2",
+    "img": "img/img_2.jpg"
+  },  {
+    "name": "MANGO  PEOPLE  T-SHIRT",
+    "price": "300",
+    "color": "Red",
+    "size": "XL",
+    "quantity": "2",
+    "img": "img/img_3.jpg"
+  },
+  {
+    "name": "ELLERY X M'O CAPSULE",
+    "price": "52",
+    "color": "Black",
+    "size": "XL",
+    "quantity": "1",
+    "img": "img/img_4.jpg"
+  }
+]`
