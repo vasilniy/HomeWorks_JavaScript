e353401117bea0d09f607426362092ee7b8f5c50
vasiliy <vasilniy@rambler.ru>
2023-02-12 22:47:28 +0600

    ДЗ поправка  №11 к Семинару 11

--- Seminar11/HomeWorks/script.js
+++ Seminar11/HomeWorks/script.js
@@ -5,17 +5,27 @@ console.log(contentEl);
 console.log(templateEl);
 
 info.product.forEach((prod) => {
-    const bottomContentItemEL = templateEl.content
-      .querySelector(".bottom_content_item")
-      .cloneNode(true);
+  const bottomContentItemEL = templateEl.content
+    .querySelector(".bottom_content_item")
+    .cloneNode(true);
 
-      const imgEl = bottomContentItemEL.querySelector(".img_fetured");       
-      imgEl.src = prod.image;
+  const imgEl = bottomContentItemEL.querySelector(".img_fetured");
+  imgEl.src = prod.image;
 
-      const createDivEl = document.createElement('div');
-      const headingFeturedEL = bottomContentItemEL.querySelector(".heading_fetured");
-      headingFeturedEL.textContent = prod.name;
+  const createDivEl = document.createElement("div");
+  const headingFeturedEL =
+    bottomContentItemEL.querySelector(".heading_fetured");
+  headingFeturedEL.textContent = prod.name;
 
-      contentEl.appendChild(bottomContentItemEL);
+  const pFeturedEL = bottomContentItemEL.querySelector(".p_fetured");
+  pFeturedEL.textContent = prod.description;
 
-    });  
+  const spanColorEL = bottomContentItemEL.querySelector(".span_color");
+  spanColorEL.textContent = prod.price + '$';
+
+  createDivEl.appendChild(headingFeturedEL);
+  createDivEl.appendChild(pFeturedEL);
+  createDivEl.appendChild(spanColorEL);
+  bottomContentItemEL.appendChild(createDivEl);
+  contentEl.appendChild(bottomContentItemEL);
+});
