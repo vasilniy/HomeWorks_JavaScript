53276d47997eb061d3c29adc0a02c8017a8d97af
vasiliy <vasilniy@rambler.ru>
2023-02-12 22:38:45 +0600

    ДЗ поправка  №11 к Семинару 11

--- Seminar11/HomeWorks/script.js
+++ Seminar11/HomeWorks/script.js
@@ -1,2 +1,21 @@
-const product_data = JSON.parse(info);
-console.log(product_data);
+const contentEl = document.querySelector(".bottom_content");
+const templateEl = document.querySelector(".template_content");
+
+console.log(contentEl);
+console.log(templateEl);
+
+info.product.forEach((prod) => {
+    const bottomContentItemEL = templateEl.content
+      .querySelector(".bottom_content_item")
+      .cloneNode(true);
+
+      const imgEl = bottomContentItemEL.querySelector(".img_fetured");       
+      imgEl.src = prod.image;
+
+      const createDivEl = document.createElement('div');
+      const headingFeturedEL = bottomContentItemEL.querySelector(".heading_fetured");
+      headingFeturedEL.textContent = prod.name;
+
+      contentEl.appendChild(bottomContentItemEL);
+
+    });  
