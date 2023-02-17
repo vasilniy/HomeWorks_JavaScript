30d8ab94698638fb51de550d967f9a00e9213ed0
vasiliy <vasilniy@rambler.ru>
2023-02-17 14:44:29 +0600

    Итоговое по  JavaScript

--- Seminar13/HomeWorks/script.js
+++ Seminar13/HomeWorks/script.js
@@ -1,3 +1,34 @@
+const contentEl = document.querySelector(".bottom_content");
+const templateEl = document.querySelector(".template_content");
+const buttonProductEL = document.querySelector(".button_product");
+
+const visibleCount = 6;
+let current = visibleCount;
+
+buttonProductEL.addEventListener('click', addProduct);
+
+function addProduct() {
+  if (current + visibleCount > info.product.length) {
+    buttonProductEL.textContent = 'Товара больше нет';
+    return;
+  }
+
+  const itemsToAdd = info.product.slice(current - visibleCount, current);
+
+  itemsToAdd.forEach((item) => {
+    const bottomContentItemEL = templateEl.content
+      .querySelector('.bottom_content_item')
+      .cloneNode(true);
+  })
+}
+
+
+
+
+
+
+
+// выпадаюее меню в навигации
 const navDropMenuEl = document.querySelector('.nav_drop_menu');
 const dropMenuEl = document.querySelector('.drop_menu');
 
