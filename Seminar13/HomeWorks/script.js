b8ae6a0af1f3a8abf33947cc721dccefe25b9ddf
vasiliy <vasilniy@rambler.ru>
2023-02-18 18:32:39 +0600

    Итоговое по  JavaScript

--- Seminar13/HomeWorks/script.js
+++ Seminar13/HomeWorks/script.js
@@ -5,17 +5,30 @@ const buttonProductEL = document.querySelector(".button_product");
 const visibleCount = 6;
 let current = visibleCount;
 addProduct();
+
 console.log(info.product.length);
 
-
 buttonProductEL.addEventListener('click', addProduct);
+
+document.addEventListener('click', e => {
+  console.log(e.target);
+});
+
+/*
+const buttonInBasketEL = document.querySelectorAll('.in_basket');
+buttonInBasketEL.forEach(element => {
+  element.addEventListener('click', e => {
+    console.log(e.target);
+  })
+});
+*/
 
 function addProduct() {  
   if (current > info.product.length) {
     buttonProductEL.textContent = 'Товара больше нет';
     return;
   }
-
+  
   const itemsToAdd = info.product.slice(current - visibleCount, current);
 
   itemsToAdd.forEach((item) => {
@@ -28,9 +41,7 @@ function addProduct() {
     bottomContentItemEL.querySelector('.heading_fetured').textContent = item.name;
     bottomContentItemEL.querySelector('.p_fetured').textContent = item.description;
     bottomContentItemEL.querySelector('.span_color').textContent = item.price;
-    bottomContentItemEL.querySelector('.img_fetured').src = item.image;
-
-    
+    bottomContentItemEL.querySelector('.img_fetured').src = item.image;    
 
     contentEl.appendChild(bottomContentItemEL);
   
